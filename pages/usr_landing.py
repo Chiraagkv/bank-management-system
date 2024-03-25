dbfec257ad029169e541b061b042517fb41e7697
Chiraagkv <69423507+Chiraagkv@users.noreply.github.com>
2024-03-25 20:36:58 +0530

    Update usr_landing.py

--- pages/usr_landing.py
+++ pages/usr_landing.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from utils import *
+import pandas as pd
 
 email = open("D:\\bank\\creds.txt").read()
 st.set_page_config(initial_sidebar_state="collapsed")
@@ -13,21 +14,104 @@ st.markdown(no_sidebar_style, unsafe_allow_html=True)
 st.markdown("# User Landing Page")
 
 option = st.selectbox(
-    "Choose your action", ("See info", "check bank schemes", "do transactions")
+    "Choose your action",
+    (
+        "Edit info",
+        "check bank schemes",
+        "Make transactions",
+        "Take a Loan",
+        "Create a Deposit",
+        "Check Your Plans",
+    ),
 )
-if option == "See info":
-    info = display_everything(email)[1:]
+if option == "Edit info":
+    info = display_everything(email)
     info_dict = {
-        "name": info[0],
-        "password": info[1],
-        "DOB": info[2],
-        "Gender": info[3],
-        "Address": info[4],
-        "Email address": info[5],
-        "Primary Phone number": info[6],
-        "Secondary Phone number": info[7],
-        "Credit Score": info[8],
+        "username": info[1],
+        "password": info[2],
+        "birth_date": info[3],
+        "gender": info[4],
+        "address": info[5],
+        "email": info[6],
+        "Primary_contact_number": info[7],
+        "Secondary_contact_number": info[8],
+        "Pin": info[-1],
     }
-    st.write(info_dict.items())
+    new_info = {}
+    for i in info_dict.items():
+        a = st.text_input(i[0], placeholder=i[1])
+        new_info[i[0]] = a
+    for i in new_info.items():
+        if i[1]:
+            info_dict[i[0]] = i[1]
+    if st.button("Done"):
+        update_user_info(info_dict, email)
+
 elif option == "check bank schemes":
-    st.write(display_schemes())
+    schemetype = st.radio("**Select loan or deposit**", ["Loans", "Deposits"])
+    all_schemes = list_schemes(schemetype)
+    table = {
+        "Scheme ID": [all_schemes[i][0] for i in range(len(all_schemes))],
+        "Scheme Name": [all_schemes[i][1] for i in range(len(all_schemes))],
+        "scheme Type": [all_schemes[i][2] for i in range(len(all_schemes))],
+        "scheme Subtype": [all_schemes[i][3] for i in range(len(all_schemes))],
+        "Interest Rate": [all_schemes[i][4] for i in range(len(all_schemes))],
+        "Tenure (Months)": [all_schemes[i][5] for i in range(len(all_schemes))],
+    }
+    df = pd.DataFrame(table)
+    st.table(df)
+
+elif option == "Take a Loan":
+    loans = list_schemes("Loans")
+    opt = st.radio("**Select loan**", loans)[0]
+    print(opt)
+    amount = st.text_input("Enter principal amount")
+    if amount:
+        info = display_everything(email)
+        pin = st.text_input("Enter account pin", type="password")
+        if info[-1] == int(pin):
+            add_investment(opt, email, float(amount))
+            st.write("Loan has been requested. We will look into it soon.")
+        elif pin and int(pin) != info[-1]:
+            st.write("Wrong pin. Try again.")
+        else:
+            st.write("")
+
+elif option == "Create a Deposit":
+    deps = list_schemes("Deposits")
+    opt = st.radio("**Select Deposit type**", deps)[0]
+    print(opt)
+    amount = st.text_input("Enter principal amount")
+    if amount:
+        info = display_everything(email)
+        pin = st.text_input("Enter account pin", type="password")
+        if info[-1] == int(pin):
+            add_investment(opt, email, float(amount))
+            st.write("Deposit will be approved soon")
+        elif pin and int(pin) != info[-1]:
+            st.write("Wrong pin. Try again.")
+        else:
+            st.write("")
+
+elif option == "Make transactions":
+    usr_acc_id = st.text_input(
+        "Enter account id (Check 'Check your plans' page to find appropriate savings account)"
+    )
+    rec_acc_id = st.text_input("Enter receiver's account_id")
+    amount = st.text_input("Enter amount to be transferred")
+    pin = st.text_input("Enter account pin", type="password")
+    if st.button("Make Payment"):
+        if (
+            check_deposit_type(int(usr_acc_id)) + check_deposit_type(int(rec_acc_id))
+            == "savingssavings"
+            and amount
+        ):
+            info = display_everything(email)
+            if info[-1] == int(pin):
+                a = change_balance(int(usr_acc_id), -int(amount))
+                if a:
+                    change_balance(int(rec_acc_id), int(amount))
+                else:
+                    st.write("Balance insufficient")
+            else:
+                st.write("Wrong password")
